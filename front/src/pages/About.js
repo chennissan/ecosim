044a8d1d7d14e6235b98dfc8dcd750c4d5ac1455
chennissan <chen.nissan@gmail.com>
2026-01-13 15:07:48 +0200

    Add WAI output (P/PET with clamp)

--- front/src/pages/About.js
+++ front/src/pages/About.js
@@ -5,11 +5,9 @@ export default function About() {
   return (
     <div>
       <center>
-      <h1><span>© 2025 Lost & Found</span></h1><br/>
-      <h2>Lost & Found GIS is a web application that helps users report and search for lost or found items across Tel Aviv, <br/><br/>
-       Using interactive maps and geolocation, the system connects people to nearby items or matching reports, <br/><br/>
-       making it easier to reunite lost belongings with their owners. <br/> <br/>
-       Powered by a fullstack architecture and GIS technology for spatial searches.</h2>
+      <h1><span>© 2026 EcoSim</span></h1><br/>
+      <h2>EcoSim is an Ecosistems Simulatoes. <br/> <br/>
+       Powered by a fullstack architecture.</h2>
        </center>
     </div>
   );
